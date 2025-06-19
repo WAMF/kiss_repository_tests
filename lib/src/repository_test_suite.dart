bc516de2cf973c8f829b838b398a9d00bcb39961
defuera <denis@devartel.io>
2025-06-19 11:59:22 +0200

    refactor: paramethrize RepositoryFactory<T>

--- lib/src/repository_test_suite.dart
+++ lib/src/repository_test_suite.dart
@@ -1,19 +1,20 @@
 // ignore_for_file: public_member_api_docs
 
 import 'package:kiss_repository/kiss_repository.dart';
-import 'package:test/test.dart';
-
 import 'package:kiss_repository_tests/src/auto_identify_tests.dart';
 import 'package:kiss_repository_tests/src/batch_tests.dart';
 import 'package:kiss_repository_tests/src/crud_tests.dart';
-import 'package:kiss_repository_tests/src/test_models.dart';
 import 'package:kiss_repository_tests/src/query_tests.dart';
 import 'package:kiss_repository_tests/src/streaming_tests.dart';
+import 'package:kiss_repository_tests/src/test_models.dart';
+import 'package:test/test.dart';
 
 // Factory interface that each repository implementation must provide
-abstract class RepositoryFactory {
-  Repository<ProductModel> createRepository();
+abstract class RepositoryFactory<T> {
+  Repository<T> createRepository();
+
   Future<void> cleanup();
+
   void dispose();
 }
 
@@ -21,11 +22,11 @@ abstract class RepositoryFactory {
 /// Following the same pattern as kiss_queue_tests
 void runRepositoryTests({
   required String implementationName,
-  required RepositoryFactory Function() factoryProvider,
+  required RepositoryFactory<ProductModel> Function() factoryProvider,
   required void Function() cleanup,
 }) {
   group('$implementationName Repository Tests', () {
-    late RepositoryFactory factory;
+    late RepositoryFactory<ProductModel> factory;
     late Repository<ProductModel> repository;
 
     setUpAll(() {
